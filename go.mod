063c7f3e484172f8362b718e079a79b3e28d2c0d
Shawn Lian <shawn@lemonvideo.com>
2023-08-04 10:00:58 +0000

    chore: initial implementation

--- go.mod
+++ go.mod
@@ -1,3 +1,24 @@
 module github.com/NOBLES5E/FeedFlux
 
 go 1.20
+
+require (
+	github.com/mmcdole/gofeed v1.2.1
+	github.com/sirupsen/logrus v1.9.3
+	github.com/urfave/cli/v2 v2.25.7
+)
+
+require (
+	github.com/PuerkitoBio/goquery v1.8.0 // indirect
+	github.com/andybalholm/cascadia v1.3.1 // indirect
+	github.com/cpuguy83/go-md2man/v2 v2.0.2 // indirect
+	github.com/json-iterator/go v1.1.12 // indirect
+	github.com/mmcdole/goxpp v1.1.0 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/modern-go/reflect2 v1.0.2 // indirect
+	github.com/russross/blackfriday/v2 v2.1.0 // indirect
+	github.com/xrash/smetrics v0.0.0-20201216005158-039620a65673 // indirect
+	golang.org/x/net v0.4.0 // indirect
+	golang.org/x/sys v0.3.0 // indirect
+	golang.org/x/text v0.5.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,9 +1,173 @@
 package main
 
 import (
-    "fmt"
+	"context"
+	"crypto/sha256"
+	"encoding/json"
+	"fmt"
+	"github.com/mmcdole/gofeed"
+	log "github.com/sirupsen/logrus"
+	"github.com/urfave/cli/v2"
+	"os"
+	"time"
 )
 
+type FeedProgress struct {
+	URL          string `json:"url"`
+	FetchedUntil int64  `json:"last_fetch_time"` // Unix timestamp
+}
+
 func main() {
-    fmt.Println("Welcome to FeedFlux!")
+	log.SetLevel(log.InfoLevel)
+	app := &cli.App{
+		Name:  "FeedFlux (ff)",
+		Usage: "parse feeds and print the contents into a unified JSON format",
+
+		Flags: []cli.Flag{
+			&cli.StringFlag{
+				Name:     "record",
+				Aliases:  []string{"r"},
+				Value:    "",
+				Usage:    "to record fetching progress, specify a progress directory",
+				Required: false,
+			},
+
+			&cli.BoolFlag{
+				Name:     "continue",
+				Aliases:  []string{"c"},
+				Usage:    "to continue fetching from a previous record, record directory must be specified",
+				Required: false,
+			},
+
+			&cli.IntFlag{
+				Name:     "timeout",
+				Usage:    "timeout in seconds for fetching a feed",
+				Value:    10,
+				Required: false,
+			},
+		},
+
+		Action: func(c *cli.Context) error {
+			recordDirPath := c.String("record")
+			continueFetch := c.Bool("continue")
+			if continueFetch && recordDirPath == "" {
+				log.Fatal("record directory must be specified when continuing fetching")
+			}
+
+			urls := c.Args().Slice()
+			results := make(chan *gofeed.Item)
+
+			for _, url := range urls {
+				go fetchFeed(url, results, recordDirPath, continueFetch, c.Int("timeout"))
+			}
+
+			for item := range results {
+				jsonFeed, _ := json.Marshal(item)
+				fmt.Println(string(jsonFeed))
+			}
+
+			return nil
+		},
+	}
+
+	if err := app.Run(os.Args); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func urlToFileName(url string) string {
+	fileName := sha256.Sum256([]byte(url))
+	return fmt.Sprintf("%x.json", fileName)
+}
+
+func readRecordFile(url string, recordDirPath string) (*FeedProgress, error) {
+	fileName := urlToFileName(url)
+	recordFilePath := fmt.Sprintf("%s/%s", recordDirPath, fileName)
+	recordFile, err := os.Open(recordFilePath)
+	// If record file does not exist, return nil with error
+	if os.IsNotExist(err) {
+		return nil, err
+	}
+	// If other error occurs, return nil with error
+	if err != nil {
+		return nil, err
+	}
+	defer recordFile.Close()
+	decoder := json.NewDecoder(recordFile)
+	var record FeedProgress
+	if err := decoder.Decode(&record); err != nil {
+		return nil, err
+	}
+	return &record, nil
+}
+
+func fetchFeed(url string, results chan *gofeed.Item, recordDirPath string, continueFetch bool, timeoutSeconds int) {
+	fp := gofeed.NewParser()
+	ctx, cancel := context.WithTimeout(context.Background(), time.Duration(timeoutSeconds)*time.Second)
+	defer cancel()
+	feed, err := fp.ParseURLWithContext(url, ctx)
+	if err != nil {
+		log.Infof("error fetching %s: %s", url, err)
+		return
+	}
+	// FetchedUntil is the largest published timestamp of all items
+	record := FeedProgress{
+		URL:          url,
+		FetchedUntil: 0,
+	}
+	for _, item := range feed.Items {
+		if item.PublishedParsed.Unix() > record.FetchedUntil {
+			record.FetchedUntil = item.PublishedParsed.Unix()
+		}
+	}
+	// record file path is sha256 of url
+	fileName := sha256.Sum256([]byte(url))
+	recordFilePath := fmt.Sprintf("%s/%x.json", recordDirPath, fileName)
+	// log feed info
+	log.WithFields(log.Fields{
+		"title": feed.Title,
+		"link":  feed.Link,
+		"items": len(feed.Items),
+	}).Infof("fetched %s", url)
+
+	lastTimeStamp := int64(0)
+	if continueFetch {
+		// If continue fetching, read record file
+		recordFile, err := readRecordFile(url, recordDirPath)
+		if err != nil {
+			log.Warnf("error reading record file %s: %s, continue fetching from scratch", recordFilePath, err)
+		} else {
+			log.Infof("continue fetching %s from %s", url, time.Unix(recordFile.FetchedUntil, 0))
+			lastTimeStamp = recordFile.FetchedUntil
+		}
+	}
+
+	for _, item := range feed.Items {
+		// If continue fetching, skip items that are fetched before
+		if continueFetch && item.PublishedParsed.Unix() <= lastTimeStamp {
+			continue
+		}
+		results <- item
+	}
+
+	if recordDirPath != "" {
+		// Create record directory if not exists
+		if _, err := os.Stat(recordDirPath); os.IsNotExist(err) {
+			if err := os.Mkdir(recordDirPath, 0755); err != nil {
+				log.Fatal(err)
+			}
+		}
+		// Write record file
+		recordFile, err := os.OpenFile(recordFilePath, os.O_CREATE|os.O_WRONLY, 0644)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer recordFile.Close()
+		encoder := json.NewEncoder(recordFile)
+		if err := encoder.Encode(record); err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	close(results)
 }
